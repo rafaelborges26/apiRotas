d87bb35a1c5d22c64b64bd745535ca9568e1c8df
Rafael Borges <rafael.borges2698@gmail.comexxxxxxit>
2019-11-18 01:56:28 -0300

    adicionando rotas de multiplas origems

--- api/rotas/index.php
+++ api/rotas/index.php
@@ -2,6 +2,7 @@
 
 
 $url = $_REQUEST;
+
     if(isset($_REQUEST))
     {
         
@@ -10,23 +11,124 @@ $url = $_REQUEST;
             {
                 
                 if($cont == 0)
-                    $origem = $url;
+                    $ori = $url;
                 else if($cont == 1)
-                    $destino = $url;
+                    $dest = $url;
                 else if($cont == 2)
-                    $autonomia = $url;
+                    $autonomia = $url; 
                 else if($cont == 3)
-                    $valorlitro = $url;
+                    $litro = $url;
                 
               $cont++;
             }
-            echo "Origem: ".$origem. "Destino: ".$destino. "Autonomia: ".$autonomia. "ValorLitro : " .$valorlitro;
-        
+
+            $autonomiaint = settype($autonomia, "float");
+            $litroint = settype($litro, "float");
+
+            setRotas($ori,$dest,$autonomia,$litro);
+
             
-        
+
+            //echo "Origem: ".$ori. "Destino: ".$dest. "Autonomia: ".$autonomia. "ValorLitro : " .$litro;
+            
+//            return json_encode()
+            //criar classe em outro arq e chamar aq para tratar os valores digitados
+
+            
     }
     else
+    {
             echo "Erro";
+    }
 
+            function setRotas(string $origem, string $destino, string $autonomia, string $valorLitro)
+            {
+
+                $aOrigem = array("A","B","A","C","B","D");
+                $aDestino = array("B","D","C","D","E","E");
+                $aKm = array(10,15,20,30,50,30);
+
+                $qtdrotas = count($aOrigem);
+                $valorcusto = 0;
+                $valorkm = 0;
+                $valorKm2 = 0;
+                $rotas = "";
+                /*
+                //1 fase - de primeira
+                for($i=0;$i<$qtdrotas;$i++)
+                {
+
+                    if($origem == $aOrigem[$i])
+                    {
+                        if($destino == $aDestino[$i])
+                        { //VER CALCULO
+                            $valorkm = $aKm[$i];
+                                $valorLitro = round($valorLitro,2);
+                                $valorcusto = $valorLitro / $autonomia * $valorkm;
+                                
+                        }
+                              
+                    }
+
+                }*/
+                    
+//                    echo "Valor litro ".$valorLitro. " Autonomia: ". $autonomia. " Valor km: ". $valorkm;
+                    
+                    //echo "Valor custo ".$valorcusto." = ".$valorLitro. " / ". $autonomia. " * ".$valorkm;
+                    //echo "Valor custo: ".$valorcusto." Origem: ".$origem. "Destino: ". $destino;
+
+                    //2 parte
+                    for($i=0;$i<$qtdrotas;$i++)
+                    {
+    
+                        if($origem == $aOrigem[$i])
+                        {
+                            $rotas = $aOrigem[$i]." ";
+                            if($destino == $aDestino[$i])
+                            { //VER CALCULO
+                                    $rotas = $rotas.$aDestino[$i]." ";
+                                    var_dump($valorkm);
+                                    $valorkm = $aKm[$i];                                    
+                                    $valorLitro = round($valorLitro,2);
+                                    $valorcusto = $valorLitro / $autonomia * $valorkm;
+                                    $i = $qtdrotas;
+                                    
+                            }else
+                            {
+                                //2 parte 
+                                
+                                //setar o destino na origem junto com o numero do indice
+                                $rotas = $rotas.$aDestino[$i]." ";
+                                
+                                for($j = 0; $j < $qtdrotas; $j++)
+                                {
+                                    if($destino == $aDestino[$j])
+                                    { 
+
+                                        if($valorKm2 > $aKm[$j] || $valorKm2 == 0)
+                                        {                                            
+                                            $valorKm2 = $aKm[$j];   
+                                        }
+                                    }
+
+                                }
+
+
+                                var_dump($valorkm);
+                                $valorkm = $valorkm + $valorKm2;
+                                $valorLitro = round($valorLitro,2);
+                                $valorcusto = $valorLitro / $autonomia * $valorkm;
+                                $rotas = $rotas.$destino." ";
+                                $i = $qtdrotas;
+                            }
+    
+                    } 
+                    
+            }
+                echo "Valor litro: ".$valorLitro. " Autonomia: ". $autonomia. " Valor km: ". $valorkm. " valor custo: ".$valorcusto ;
+                echo " Rotas: ". $rotas;
+
+            
+        }
     
 ?>
